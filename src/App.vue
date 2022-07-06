ca7bc1c87d13646ae2d7e654eb564f339c06dbcf
Vincenzo- <viulenzgg@gmail.com>
2022-07-06 11:13:51 +0200

    Added Welcome Screen / Exercise 5

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,23 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,400;1,500&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+button {
+  font-family: inherit;
+  font-weight: 500;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Poppins;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: teal;
 }
+
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,17 @@
 <template>
-  <Buttons @changePhrase="changePhrase" />
-  <Scene :story="story" :sentence="currentSentence" />
+  <div v-if="showSplash" id="splash">
+    <h1>Welcome to Planet Monchi 141</h1>
+    <p>
+      This is the story of Subject 41. His quest was like none other. <br />
+      A new video game he really liked just came out so he just HAD to get home and play it. <br />
+      This is how it went down...
+    </p>
+    <button @click="toggleCanvass">START YOUR JOURNEY</button>
+  </div>
+  <div v-if="showCanvass">
+    <Buttons @changePhrase="changePhrase" />
+    <Scene :story="story" :sentence="currentSentence" />
+  </div>
 </template>
 
 <script>
@@ -15,6 +26,8 @@ export default {
     return {
       story: ["Our hero was floating in outer space when in the distance he spotted a spaceship,", "He was curious about the interior of the ship and began to inspect it. He came to a room with two doors.", "The hero decided to go through the door that took him home,", "Meanwhile, other heroes weren't so lucky in their choice..."],
       currentSentence: 0,
+      showSplash: true,
+      showCanvass: false,
     };
   },
   methods: {
@@ -28,6 +41,50 @@ export default {
       if (this.currentSentence < 0) this.currentSentence = 3;
       if (this.currentSentence > 3) this.currentSentence = 0;
     },
+
+    toggleCanvass() {
+      this.showCanvass = !this.showCanvass;
+      this.showSplash = !this.showSplash;
+    },
   },
 };
 </script>
+
+<style>
+#splash {
+  background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.6)), url("../assets/img/bg1.jpg") center/cover;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+}
+#splash h1 {
+  color: crimson;
+  text-transform: uppercase;
+  font-weight: 600;
+  text-shadow: 6px 4px 1px white;
+  font-size: 5rem;
+}
+#splash p {
+  font-size: 1.7rem;
+  margin: 2rem 0;
+  color: white;
+  text-shadow: 1px 1px 1px crimson;
+}
+
+#splash button {
+  font-size: 1.6rem;
+  padding: 1.5rem;
+  border-radius: 11px;
+  border: none;
+  cursor: crosshair;
+  background-color: crimson;
+  color: white;
+}
+
+#splash button:active {
+  transform: scale(0.96);
+}
+</style>
